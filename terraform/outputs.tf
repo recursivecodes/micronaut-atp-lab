67a45a8b01d112e6c4d5a95120ddaf390163f5c5
recursivecodes <toddraymondsharp@gmail.com>
2020-07-29 15:38:20 -0400

    clean up the outputs a bit - remove some unnecessary output.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,39 +3,9 @@ output "vcn_id" {
   value       = oci_core_vcn.this.id
 }
 
-output "default_security_list_id" {
-  description = "OCID of default security list. "
-  value       = oci_core_vcn.this.default_security_list_id
-}
-
-output "default_dhcp_options_id" {
-  description = "OCID of default DHCP options. "
-  value       = oci_core_vcn.this.default_dhcp_options_id
-}
-
-output "default_route_table_id" {
-  description = "OCID of default route table. "
-  value       = oci_core_vcn.this.default_route_table_id
-}
-
-output "internet_gateway_id" {
-  description = "OCID of internet gateway. "
-  value       = oci_core_internet_gateway.this.id
-}
-
-output "subnet_ids" {
-  description = "ocid of subnet ids. "
-  value       = oci_core_subnet.this.*.id
-}
-
 output "instance_id" {
   description = "ocid of created instances. "
   value       = [oci_core_instance.this.id]
-}
-
-output "private_ip" {
-  description = "Private IPs of created instances. "
-  value       = [oci_core_instance.this.private_ip]
 }
 
 output "public_ip" {
@@ -51,32 +21,12 @@ output "autonomous_database_schema_password" {
   value = random_string.autonomous_database_schema_password.result
 }
 
-output "autonomous_database_high_connection_string" {
-  value = lookup(oci_database_autonomous_database.autonomous_database.connection_strings.0.all_connection_strings, "high", "unavailable")
-}
-
-output "autonomous_databases" {
-  value = data.oci_database_autonomous_databases.autonomous_databases.autonomous_databases
-}
-
 output "autonomous_database_wallet_password" {
   value = random_string.autonomous_database_wallet_password.result
 }
 
-output "security_list" {
-  value = oci_core_security_list.this
-}
-
-output "vault" {
-  value = oci_kms_vault.this
-}
-
 output "vault_id" {
   value = oci_kms_vault.this.id
-}
-
-output "key" {
-  value = oci_kms_key.this
 }
 
 output "key_id" {
