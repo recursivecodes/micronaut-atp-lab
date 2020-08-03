60296a2f60804573908888a675a5d885310147ca
recursivecodes <toddraymondsharp@gmail.com>
2020-08-03 16:57:23 -0400

    update to choose the proper ad for always free

--- terraform/main.tf
+++ terraform/main.tf
@@ -83,7 +83,7 @@ data "oci_limits_limit_values" "ad_limits" {
   count          = length(data.oci_identity_availability_domains.this.availability_domains)
   compartment_id = var.tenancy_ocid
   service_name   = data.oci_limits_services.services.services.0.name
-  availability_domain = data.oci_identity_availability_domains.this[count.index].name
+  availability_domain = data.oci_identity_availability_domains.this.availability_domains[count.index].name
   name                = "vm-standard-e2-1-micro-count"
   scope_type          = "AD"
 }
