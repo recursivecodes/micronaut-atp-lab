e040085d9656806564ac4da676af91b6570a1b2b
recursivecodes <toddraymondsharp@gmail.com>
2020-08-04 12:06:33 -0400

    final fix for cloud init. java (graalvm) properly installs, firewall ports are properly opened.

--- scripts/cloud-init.sh
+++ scripts/cloud-init.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 # this script is used to generate the "user_data" in `setup.sh`
 # base64 encode this file and set it in terraform/variables.tf, and whatever is in this script will be run at startup of the VM
-echo "***setting firewall rules..."
+echo "***making /java directory***"
+mkdir -p /java
+echo "***downloading graalvm***"
+wget -q -O /java/graalvm-ce-java11-linux-amd64-20.1.0.tar.gz https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-20.1.0/graalvm-ce-java11-linux-amd64-20.1.0.tar.gz
+echo "***unzipping graalvm***"
+tar -xf /java/graalvm-ce-java11-linux-amd64-20.1.0.tar.gz -C /java/
+echo "***removing zip***"
+rm /java/graalvm-ce-java11-linux-amd64-20.1.0.tar.gz
+echo "***setting JAVA_HOME***"
+echo "export JAVA_HOME=/java/graalvm-ce-java11-20.1.0" >> /etc/profile.d/java.sh
+echo "***setting PATH***"
+echo "export PATH=\$JAVA_HOME/bin:\$PATH" >> /etc/profile.d/java.sh
+echo "***setting firewall rules***"
 sudo firewall-offline-cmd --zone=public --add-port=8080/tcp
-echo "***downloading graalvm..."
-wget -q -O /home/opc/graalvm-ce-java11-linux-amd64-20.1.0.tar.gz https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-20.1.0/graalvm-ce-java11-linux-amd64-20.1.0.tar.gz
-echo "***unzipping graalvm..."
-tar -xf /home/opc/graalvm-ce-java11-linux-amd64-20.1.0.tar.gz
-echo "***removing zip..."
-rm /home/opc/graalvm-ce-java11-linux-amd64-20.1.0.tar.gz
-echo "***setting JAVA_HOME..."
-export JAVA_HOME=/home/opc/graalvm-ce-java11-20.1.0
-echo "***setting PATH..."
-export PATH=$JAVA_HOME/bin:$PATH
-echo "***setting motd..."
-sudo sh -c 'echo "Welcome Micronaut HOL Attendee!" > /etc/motd'
-echo "setup done."
+sudo systemctl restart firewalld
+echo "***setting motd***"
+sudo sh -c 'echo "Welcome Micronaut Hands On Lab Attendee!" > /etc/motd'
+echo "setup done!"
