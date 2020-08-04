351e4947ea18e3e19cd477780e3818f4fa6ddb40
recursivecodes <toddraymondsharp@gmail.com>
2020-08-04 10:44:53 -0400

    simplify variable input screen.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "instance_display_name" {
   default = "mn-oci-demo"
 }
 variable "boot_volume_size_in_gbs" {
-  default = 40
+  default = 50
 }
 variable "shape" {
   default = "VM.Standard.E2.1.Micro"
@@ -23,9 +23,6 @@ variable "user_data" {
   default = "IyEvYmluL3NoCiMgdGhpcyBzY3JpcHQgaXMgdXNlZCB0byBnZW5lcmF0ZSB0aGUgInVzZXJfZGF0YSIgaW4gYHNldHVwLnNoYAojIGJhc2U2NCBlbmNvZGUgdGhpcyBmaWxlIGFuZCBzZXQgaXQgaW4gdGVycmFmb3JtL3ZhcmlhYmxlcy50ZiwgYW5kIHdoYXRldmVyIGlzIGluIHRoaXMgc2NyaXB0IHdpbGwgYmUgcnVuIGF0IHN0YXJ0dXAgb2YgdGhlIFZNCnN1ZG8gZmlyZXdhbGwtb2ZmbGluZS1jbWQgLS16b25lPXB1YmxpYyAtLWFkZC1wb3J0PTgwODAvdGNwCndnZXQgLXEgLU8gL2hvbWUvb3BjL2dyYWFsdm0tY2UtamF2YTExLWxpbnV4LWFtZDY0LTIwLjEuMC50YXIuZ3ogaHR0cHM6Ly9naXRodWIuY29tL2dyYWFsdm0vZ3JhYWx2bS1jZS1idWlsZHMvcmVsZWFzZXMvZG93bmxvYWQvdm0tMjAuMS4wL2dyYWFsdm0tY2UtamF2YTExLWxpbnV4LWFtZDY0LTIwLjEuMC50YXIuZ3oKdGFyIC14ZiAvaG9tZS9vcGMvZ3JhYWx2bS1jZS1qYXZhMTEtbGludXgtYW1kNjQtMjAuMS4wLnRhci5negpybSAvaG9tZS9vcGMvZ3JhYWx2bS1jZS1qYXZhMTEtbGludXgtYW1kNjQtMjAuMS4wLnRhci5negpleHBvcnQgSkFWQV9IT01FPS9ob21lL29wYy9ncmFhbHZtLWNlLWphdmExMS0yMC4xLjAKZXhwb3J0IFBBVEg9JEpBVkFfSE9NRS9iaW46JFBBVEgKc3VkbyBzaCAtYyAnZWNobyAiV2VsY29tZSBNaWNyb25hdXQgSE9MIEF0dGVuZGVlISIgPiAvZXRjL21vdGQnCg=="
 }
 
-variable "block_storage_size_in_gbs" {
-  default = 50
-}
 variable "vcn_display_name" {
   default = "testVCN"
 }
